0304c549d650bef1e87a8c2a3900a54b0726ee33
jason60810 <jason60810@gmail.com>
2022-10-21 19:52:51 +0800

    build: init and add Vue Router

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,5 @@
-import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
-import App from "./App.vue";
-import ViewNotes from "@/views/ViewNote.vue";
-import ViewStats from "@/views/ViewStats.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "notes",
-      component: ViewNotes,
-    },
-    {
-      path: "/stats",
-      name: "stats",
-      component: ViewStats,
-    },
-  ],
-});
-
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).mount('#app');
