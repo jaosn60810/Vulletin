ad03223db82952fe3c98a2f1694a6076134cb16a
jason60810 <jason60810@gmail.com>
2022-10-21 21:12:30 +0800

    test squash3

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,6 +1,5 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    test1
     <div class="container is-max-desktop">
       <div class="navbar-brand">
         <h1 class="navbar-item is-size-4 is-family-monospace">Vulletin</h1>
