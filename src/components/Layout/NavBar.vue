a7d9e25562861ebc31de97b526f7854e0e5f9b6e
jason60810 <jason60810@gmail.com>
2022-10-21 21:08:53 +0800

    feat: add nav-bar

---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <h1 class="navbar-item is-size-4 is-family-monospace">Vulletin</h1>
+
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showMobileNav }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMobileNav }"
+      >
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item">Notes</RouterLink>
+          <RouterLink to="stats" class="navbar-item">Stats</RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const showMobileNav = ref(false);
+</script>
+
+<style scoped>
+.navbar-menu {
+  position: absolute;
+  left: 0;
+  width: 100%;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,7 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  linkActiveClass: 'is-active',
 });
 
 export default router;
